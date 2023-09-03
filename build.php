954f8102ea3291f2d134060392ceace5063e4af7
Superkooka <aymeric.gueracague@gmail.com>
2023-09-03 07:16:06 +0200

    build + homepage
    Signed-off-by: Superkooka <aymeric.gueracague@gmail.com>

---
+++ build.php
@@ -0,0 +1,33 @@
+<?php
+
+print "[INFO] Importing all quizz in quiz/.\n";
+$quizz = array_map(fn ($filename) => require($filename), glob("quiz/*.php"));
+print "[INFO] " . count($quizz) . " quizz found.\n";
+
+if (is_dir("build/")) {
+  print "[INFO] Cleaning build/ folder\n";
+  $rmdir = exec("rm -rf build/"); // sry
+}
+
+mkdir("build/");
+
+$quizzHtmlList = "";
+foreach ($quizz as $quiz) {
+  $quizzHtmlList .= sprintf(<<<HTML
+<li class="my-4"><a href="%s.html" class="text-xl underline">%s</a></li>
+HTML, $quiz["slug"], $quiz["name"]);
+}
+
+$homepage_template = file_get_contents("template/homepage.html");
+$homepage_file = str_replace("##QUIZ_LIST##", $quizzHtmlList, $homepage_template);
+file_put_contents("build/index.html", $homepage_file);
+
+
+$question_template = file_get_contents("template/question.html");
+foreach ($quizz as $quiz) {
+  foreach ($quiz["questions"] as $question) {
+    $question_file = str_replace("##QUIZ_NAME##", $quiz["name"], $question_template);
+
+    file_put_contents(sprintf("build/%s.html", $quiz["slug"]), $question_file);
+  }
+}
---
+++ quiz/communism_or_capitalism.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  "slug" => "communism", 
+  "name" => "Êtes vous Communiste?",
+
+  "questions" => [
+    ["name" => "Devrait on instauré une dictature prolétarienne?", "weight" => 2 ],
+    ["name" => "Les classes ouvrières du monde entier ont plus de points communs entre elles qu'avec les classes non ouvrières de leur propre pays.", "weight" => 1],
+  ],
+];
